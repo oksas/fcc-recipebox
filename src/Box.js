2902daf1f047424312e9dc138d1be5fdf08cdf9f
oksas <john@oksasoglu@gmail.com>
2016-08-04 00:05:35 -0700

    Refine styles of <RecipeEditor>

--- src/Box.js
+++ src/Box.js
@@ -2,51 +2,51 @@ import React from "react";
 import { Motion, spring } from "react-motion";
 
 class Box extends React.Component {
-  render() {
-    const { width, margin, units } = this.props.gridConfig;
-    const { index, count, item } = this.props;
-    const y = (width + margin) * index;
-    const style = {
-      translateY: spring(y)
-    };
-    return (
-      <Motion key={item.id} style={style}>
-				{ ({translateY, backgroundColor}) =>
+	render() {
+		const { width, margin, units } = this.props.gridConfig;
+		const { index, count, item } = this.props;
+		const y = (width + margin) * index;
+		const style = {
+			translateY: spring(y)
+		};
+		return (
+			<Motion key={item.id} style={style}>
+			{ ({translateY, backgroundColor}) =>
 					<div
 						onClick={this.props.handleClick.bind(null, index)}
             className={index === 0 ? "active-box" : "box"}
-            style={{
-              backgroundColor: item.color,
-              zIndex: index === 0 ? 99 : count - index,
-              transform: `translate3d(0, ${translateY}${units}, 0)`,
-              width: `${width}${units}`,
-              height: `${width}${units}`
-            }}>
+						style={{
+							backgroundColor: item.color,
+							zIndex: index === 0 ? 99 : count - index,
+							transform: `translate3d(0, ${translateY}${units}, 0)`,
+							width: `${width}${units}`,
+							height: `${width}${units}`
+						}}>
 						<span className="box-label">{this.props.item.title[0]}</span>
 					</div>
 				}
 			</Motion>
     );
-  }
+	}
 }
 
 Box.propTypes = {
-  gridConfig: React.PropTypes.shape({
-    rowLen: React.PropTypes.number,
-    margin: React.PropTypes.number,
-    width: React.PropTypes.number,
-    units: React.PropTypes.string
-  }).isRequired,
-  item: React.PropTypes.shape({
-    id: React.PropTypes.number,
-    color: React.PropTypes.string,
-    title: React.PropTypes.string,
-    ingredients: React.PropTypes.array,
-    instructions: React.PropTypes.array
-  }).isRequired,
-  index: React.PropTypes.number.isRequired,
-  count: React.PropTypes.number.isRequired,
-  handleClick: React.PropTypes.func.isRequired
+	gridConfig: React.PropTypes.shape({
+		rowLen: React.PropTypes.number,
+		margin: React.PropTypes.number,
+		width: React.PropTypes.number,
+		units: React.PropTypes.string
+	}).isRequired,
+	item: React.PropTypes.shape({
+		id: React.PropTypes.number,
+		color: React.PropTypes.string,
+		title: React.PropTypes.string,
+		ingredients: React.PropTypes.array,
+		instructions: React.PropTypes.array
+	}).isRequired,
+	index: React.PropTypes.number.isRequired,
+	count: React.PropTypes.number.isRequired,
+	handleClick: React.PropTypes.func.isRequired
 };
 
 export default Box;
--- src/RecipeEditor.js
+++ src/RecipeEditor.js
@@ -3,42 +3,43 @@ import CSSModules from "react-css-modules";
 import styles from "./styles/recipe-editor.scss";
 
 class RecipeEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { item: this.props.initialItem };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSave = this.handleSave.bind(this);
-    this.handleCancel = this.handleCancel.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = { item: this.props.initialItem };
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSave = this.handleSave.bind(this);
+		this.handleCancel = this.handleCancel.bind(this);
+	}
 
-  handleChange(event) {
-    const field = event.target.name;
-    let value;
+	handleChange(event) {
+		const field = event.target.name;
+		let value;
 
-    if (field === "ingredients" || field === "instructions") {
-      value = event.target.value.split("\n");
-    } else {
-      value = event.target.value;
-    }
+		if (field === "ingredients" || field === "instructions") {
+			value = event.target.value.split("\n");
+		} else {
+			value = event.target.value;
+		}
 
-    this.setState({
-      item: {
-        ...this.state.item,
-        [field]: value
-      }
-    });
-  }
+		this.setState({
+			item: {
+				...this.state.item,
+				[field]: value
+			}
+		});
+	}
 
-  handleSave() {
-    this.props.onSave(this.state.item);
-  }
+	handleSave() {
+		this.props.onSave(this.state.item);
+	}
 
-  handleCancel() {
-    this.props.onCancel();
-  }
+	handleCancel() {
+		this.props.onCancel();
+	}
 
-  render() {
-    return (
+	render() {
+		// This should fade in and fall from the top, subtly, like on my site, just in reverse
+		return (
       <div styleName="wrapper">
         <div styleName="editor">
           <div styleName="title">
@@ -65,24 +66,30 @@ class RecipeEditor extends React.Component {
               onChange={this.handleChange}
               value={this.state.item.instructions.join("\n")} />
           </div>
-          <button onClick={this.handleSave}>save</button>
-          <button onClick={this.handleCancel}>cancel</button>
+          <button styleName="action" onClick={this.handleSave}>
+						<span styleName="icon">-</span>
+						save
+					</button>
+          <button styleName="action" onClick={this.handleCancel}>
+						<span styleName="icon">-</span>
+						cancel
+					</button>
         </div>
       </div>
 		);
-  }
+	}
 }
 
 RecipeEditor.propTypes = {
-  initialItem: React.PropTypes.shape({
-    id: React.PropTypes.number,
-    color: React.PropTypes.string,
-    title: React.PropTypes.string,
-    ingredients: React.PropTypes.array,
-    instructions: React.PropTypes.array
-  }).isRequired,
-  onSave: React.PropTypes.func.isRequired,
-  onCancel: React.PropTypes.func.isRequired
+	initialItem: React.PropTypes.shape({
+		id: React.PropTypes.number,
+		color: React.PropTypes.string,
+		title: React.PropTypes.string,
+		ingredients: React.PropTypes.array,
+		instructions: React.PropTypes.array
+	}).isRequired,
+	onSave: React.PropTypes.func.isRequired,
+	onCancel: React.PropTypes.func.isRequired
 };
 
 export default CSSModules(RecipeEditor, styles);
